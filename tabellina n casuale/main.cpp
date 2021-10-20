dd5424684c17803a1f5d9a37f8297d29048008b2
Ignorance <erenblaze32@gmail.com>
2021-10-20 09:36:54 +0200

    tabellina n casuale commit 1
    non finito

---
+++ tabellina n casuale/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+    int numero;
+    int i;
+
+    //estrazioni
+    srand(time(0));
+    numero=rand()%10+1;
+    for (i=0; i<10; numero=){
+
+    }
+
+
+    return 0;
+}
