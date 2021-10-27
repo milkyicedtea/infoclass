10c80d51683f6f694a1c72281c25153c41d7e874
ignorance-uwu <erenblaze32@gmail.com>
2021-10-27 15:11:33 +0200

    Competizione cani
    Competizione cani finito
    Voti alunni wip

---
+++ Competizione cani/main.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int npettorina;
+    char sesso;
+    string razza;
+    int punti;
+    int eta;
+    float media;
+    int nmaschi=0;
+    int maxpunti=-10;
+    int nbarboncini=0;
+    int pettorinavincente;
+    int ncani=1;
+
+    cout << "Inserire il numero totale dei cani" << endl;
+    cin >> ncani;
+
+    for (npettorina=1; npettorina<ncani+1; npettorina++){
+    cout << "Inserire l'eta' del cane" << endl;
+    cin >> eta;
+    cout << "Inserire il sesso del cane" << endl;
+    cin >> sesso;
+    cout << "Inserire i punti accumulati dal cane" << endl;
+    cin >> punti;
+    cout << "Inserire la razza del cane" << endl;
+    cin >> razza;
+    if (maxpunti < punti)
+        {
+            maxpunti = punti;
+            pettorinavincente = npettorina;
+        }
+    if (sesso == 'm')
+        {
+            nmaschi++;
+        }
+    if (razza == "barboncino")
+        {
+            nbarboncini++;
+        }
+        else if (razza == "labrador")
+            {
+                cout << "Pettorina labrador n: " << npettorina << endl;
+            }
+    media = media + eta;
+}
+    //calcolo media
+    media = media/ncani;
+    cout << "La media delle eta' e' di: " << media << endl;
+    cout << "Ci sono " << nmaschi << " maschi." << endl;
+    cout << "Ci sono " << nbarboncini << " barboncini." << endl;
+    cout << "Il vincitore e' il cane con la pettorina n: " << pettorinavincente;
+
+
+    return 0;
+}
---
+++ Voti alunni/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int nstudente;
+    string materia;
+    int nvoti=6;
+    int voto;
+    char tipo;
+    float media;
+    int nsufficienti;
+    int maggioridi9;
+
+    for (nstudente=0; nstudente<nvoti+1; nstudente++){
+        int i;
+        srand(time(0));
+        for (i=0; i<nvoti+1; i++){
+            voto=rand()%10+1;
+            media=media+voto;
+
+
+    }}
+
+
+
+
+
+
+
+
+    return 0
+}
