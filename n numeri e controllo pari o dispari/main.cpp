bc30310b761908034637619e27147970ac00078c
Ignorance <81954613+ignorance-uwu@users.noreply.github.com>
2021-10-28 11:27:03 +0200

    n num randomici e controllo pari o dispari
     da finire

---
+++ n numeri e controllo pari o dispari/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Inserire la quantita' di numeri da analizzare" << endl;
+    cin >> nnumeri;
+
+        for (i=1; i<nvoti+1; i++){
+            voto=rand()%10+1;
+
+
+
+
+
+
+
+    return 0;
+}
