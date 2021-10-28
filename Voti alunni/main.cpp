a79ebabab2f2dae124e3fdf905998ff18dedfbb9
Ignorance <81954613+ignorance-uwu@users.noreply.github.com>
2021-10-28 10:41:37 +0200

    Voti alunni
    Finito voti alunni

--- Voti alunni/main.cpp
+++ Voti alunni/main.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
@@ -10,25 +13,56 @@ int main()
     int voto;
     char tipo;
     float media;
-    int nsufficienti;
-    int maggioridi9;
+    int nsufficienti=0;
+    int maggioridi9=0;
+    int studentitotali;
+    int maxmate=-10;
+    int i;
 
-    for (nstudente=0; nstudente<nvoti+1; nstudente++){
-        int i;
+    cout << "Inserire gli studenti totali" << endl;
+    cin >> studentitotali;
+
+    for (nstudente=1; nstudente<studentitotali+1; nstudente++){
         srand(time(0));
-        for (i=0; i<nvoti+1; i++){
+        cout << "Studente numero " << nstudente << endl;
+        for (i=1; i<nvoti+1; i++){
             voto=rand()%10+1;
             media=media+voto;
+            if (i>=2)
+            {
+                media=media/2;
+            }
+            cout << "Inserire la materia" << endl;
+            cin >> materia;
+            cout << "Inserire il tipo di voto" << endl;
+            cin >> tipo;
+            if (voto >= 6 )
+                {
+                    nsufficienti++;
+                }
+            if (voto > 9 && tipo == 's' || voto > 9 && tipo == 'p')
+                {
+                    maggioridi9++;
+                }
 
+            if (materia == "matematica" && voto > maxmate)
+            {
+                maxmate=voto;
+            } else if (materia == "inglese")
+                {
+                    cout << "Voto " << tipo << " di inglese: " << voto << endl;
+                }
 
-    }}
+        }
+}
+    if (maxmate = -10)
+    {
+        maxmate = 0;
+    }
+    cout << "La media dello studente e' di: " << media << endl;
+    cout << "Il massimo dei voto in matematica e': " << maxmate << endl;
+    cout << "Il totale delle verifiche sufficienti e': " << nsufficienti << endl;
+    cout << "I voti scritti e pratici maggiori di nove sono: " << maggioridi9 << endl;
 
-
-
-
-
-
-
-
-    return 0
+    return 0;
 }
