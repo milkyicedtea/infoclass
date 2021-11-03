5711c6538447e6b2742fd75cffb3f731eccd4cfa
ignorance-uwu <erenblaze32@gmail.com>
2021-11-03 22:19:13 +0100

    something

--- tabellina n casuale/main.cpp
+++ tabellina n casuale/main.cpp
@@ -8,14 +8,15 @@ int main()
 {
     int numero;
     int i;
+    int posizione=1;
+    int risultato=0;
 
-    //estrazioni
     srand(time(0));
     numero=rand()%10+1;
-    for (i=0; i<10; numero=){
-
+    for (i=0; i<10; i++, posizione++){
+        risultato = numero*posizione;
+        cout << risultato << " " << endl;
     }
-
 
     return 0;
 }
