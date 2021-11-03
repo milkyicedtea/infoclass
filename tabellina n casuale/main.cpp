3c155a7f8681c6a87e6e898e7c28ed570ea638b0
ignorance-uwu <erenblaze32@gmail.com>
2021-11-03 22:21:22 +0100

    Update main.cpp

--- tabellina n casuale/main.cpp
+++ tabellina n casuale/main.cpp
@@ -13,9 +13,23 @@ int main()
 
     srand(time(0));
     numero=rand()%10+1;
+<<<<<<< Updated upstream
     for (i=0; i<10; i++, posizione++){
         risultato = numero*posizione;
         cout << risultato << " " << endl;
+=======
+<<<<<<< HEAD
+    cout << "Numero iniziale: " << numero << endl;
+    cout << "\nTabellina: " << endl;
+    for (i=0; i<10; i++){
+        numero=numero*(numero/numero);
+        cout << numero << endl;
+=======
+    for (i=0; i<10; i++, posizione++){
+        risultato = numero*posizione;
+        cout << risultato << " " << endl;
+>>>>>>> 5711c6538447e6b2742fd75cffb3f731eccd4cfa
+>>>>>>> Stashed changes
     }
 
     return 0;
