6849bb5047747f81e20caa3409b4394dee2aa4fd
Ignorance <81954613+ignorance-uwu@users.noreply.github.com>
2021-12-16 10:36:08 +0100

    functions
    Esercizio tombola da finire

---
+++ Esercizio tombola/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+    const int La = 3;
+    const int Lu = 5;
+    int g1c1[Lu][La];
+    int g2c1[Lu][La];
+    int valorepescato = 0;
+    int tempcartella = 0;
+    int row = 0;
+    int col = 0;
+    int estrazioni = 0;
+    int nestratto = 0;
+
+    srand(time(0));
+    for (row = 0, col = 0; row > La; i++) //Riempi g1
+    tempcartella = rand()%100+1;
+    g1c1[row][col] = tempcartella
+    if (row == 4)
+    {
+        col++;
+    }
+    for (row = 0, col = 0; row > La; i++) //Riempi g2
+    tempcartella = rand()%100+1;
+    g2c1[row][col] = tempcartella
+    if (row == 4)
+    {
+        col++;
+    }
+    for (estrazioni = 0; estrazioni == 15; estrazioni++)
+    nestratto = rand()%100+1;
+    
+
+    
+
+
+
+
+
+
+
+    return 0;
+}
---
+++ functions/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+void ciao()
+{
+    cout << "ciao" << endl;
+    cout << "porco dio" << endl;
+}
+
+int main()
+{
+    ciao();
+}
