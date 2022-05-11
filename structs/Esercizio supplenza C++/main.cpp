a6cd7ff5ea527ca0c9ccd8f7b256513867473bec
Ignorance <erenblaze32@gmail.com>
2022-05-11 23:30:32 +0200

    C++ start file so i can continue at school

---
+++ structs/Esercizio supplenza C++/main.cpp
@@ -0,0 +1,100 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+//const per for e array
+const int N = 15;
+
+struct prodotto
+{
+    string nome = "";
+    float prezzo = 0; 
+    int quantita = 0;
+    string reparto = "";
+    int codice = 0;
+};
+
+//crea array di 'prodotto'
+prodotto prodotti[N] = {};
+
+void carica_prodotto()
+{
+
+}
+
+void print_prodotto()
+{
+
+}
+
+float calcolo_minimo()
+{
+
+}
+
+int totale_giacenza()
+{
+
+}
+
+int prodotti_banco()
+{
+
+}
+
+void prezzo_superiore_a()
+{
+
+}
+
+float media_casalinghi()
+{
+
+}
+
+void quantita_magg_20()
+{
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int main()
+{
+
+
+
+
+
+
+    return 0;
+}
