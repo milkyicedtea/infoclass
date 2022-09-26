25b758d36e771b46722476dc74cc486cbcb2fc9a
Ignorance <81954613+milkyicedtea@users.noreply.github.com>
2022-09-26 13:55:28 +0200

    Create Vini.cpp

---
+++ Secondo anno/Vini.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstdlib>
+
+struct Vini
+
+{
+  string denominazione;
+  string colore;
+  string origine;
+  int anno_imbottigliamento;
+  float gradazione;
+  float prezzo;
+};
+
+Vini pointer1 = NULL;
+Vini pointer2 = NULL;
+Vini pointer3 = NULL;
+Vini pointer4 = NULL;
+    
+  
