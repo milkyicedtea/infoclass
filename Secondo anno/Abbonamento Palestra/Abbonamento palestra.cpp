e4e30104057d853aa665d4057ab623cbf4e37bce
Ignorance <erenblaze32@gmail.com>
2022-10-06 23:05:34 +0200

    finito abbonamento palestra.cpp

---
+++ Secondo anno/Abbonamento Palestra/Abbonamento palestra.cpp
@@ -0,0 +1,161 @@
+//struttura
+//inserimento testa
+//inserimento coda
+//calcolo importo
+
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+
+struct Abbonamento
+{
+    string codice_fiscale = "";
+    int durata = 0;
+    string tipo = "";
+    float prezzo = 0;
+    Abbonamento* prox = NULL;
+};
+
+Abbonamento* testa = NULL;
+
+void CreaLista(string codice_f, int durata_abb, string tipo_abb, float prezzo_abb)
+{
+    Abbonamento* nuovo = NULL;
+    nuovo = new(Abbonamento);
+    nuovo->codice_fiscale = codice_f;
+    nuovo->durata = durata_abb;
+    nuovo->tipo = tipo_abb;
+    nuovo->prezzo = prezzo_abb;
+    nuovo->prox = NULL;
+    testa = nuovo;
+}
+
+void InsTesta(string codice_f, int durata_abb, string tipo_abb, float prezzo_abb)
+{
+    Abbonamento* nuovo = NULL;
+    nuovo = new(Abbonamento);
+    nuovo->codice_fiscale = codice_f;
+    nuovo->durata = durata_abb;
+    nuovo->tipo = tipo_abb;
+    nuovo->prezzo = prezzo_abb;
+    nuovo->prox = testa;
+    testa = nuovo;
+}
+
+void InsCoda(string codice_f, int durata_abb, string tipo_abb, float prezzo_abb)
+{
+    Abbonamento* nuovo = NULL;
+    Abbonamento* app = NULL;
+    nuovo = new(Abbonamento);
+    nuovo->codice_fiscale = codice_f;
+    nuovo->durata = durata_abb;
+    nuovo->tipo = tipo_abb;
+    nuovo->prezzo = prezzo_abb;
+    nuovo->prox = NULL;
+    if (testa == NULL)
+    {
+        InsTesta(codice_f, durata_abb, tipo_abb, prezzo_abb);
+    }
+    else
+    {
+        app = testa;
+        while (app->prox != NULL)
+        {
+            app = app->prox;
+        }
+        app->prox = nuovo;
+    }
+}
+
+void CalcolaImportiTotali()
+{
+    float importo_totale = 0.0;
+
+    Abbonamento* app = NULL;
+    if (testa == NULL)
+    {
+        cout << "Your list is empty" << endl;
+    }
+    else
+    {
+        app = testa;
+        while (app != NULL)
+        {
+            importo_totale += app->prezzo;
+            app = app->prox;
+        }
+        cout << "Il totale degli importi e': " << importo_totale << endl;
+    }
+}
+
+int main()
+{
+    string codice_fiscale = "";
+    int durata = 0;
+    string tipo = "";
+    float prezzo = 0;
+    int scelta = 10;
+    do
+    {
+        cout << "Inserire 0 per uscire;\nInserire 1 per inizializzare una lista;\nInserire 2 per inserire un abbonamento in cima alla lista;\nInserire 3 per inserire un abbonamento in fondo alla lista;\nInserire 4 per stampare il valore dell'importo totale;" << endl;
+        cin >> scelta;
+        switch (scelta)
+        {
+        case 0:
+            cout << "Uscendo dal programma.." << endl;
+            break;
+
+        case 1:
+            cout << "Inserire il primo abbonamento della nuova lista" << endl;
+            cout << "\nInserire il proprio codice fiscale:" << endl;
+            cin >> codice_fiscale;
+            cout << "\nInserire la durata dell'abbonamento:" << endl;
+            cin >> durata;
+            cout << "\nInserire il tipo di abbonamento:" << endl;
+            cin >> tipo;
+            cout << "\nInserire il prezzo dell'abbonamento:" << endl;
+            cin >> prezzo;
+            CreaLista(codice_fiscale, durata, tipo, prezzo);
+            break;
+
+        case 2:
+            cout << "Inserire l'abbonamento da inserire in cima alla lista" << endl;
+            cout << "\nInserire il proprio codice fiscale:" << endl;
+            cin >> codice_fiscale;
+            cout << "\nInserire la durata dell'abbonamento:" << endl;
+            cin >> durata;
+            cout << "\nInserire il tipo di abbonamento:" << endl;
+            cin >> tipo;
+            cout << "\nInserire il prezzo dell'abbonamento:" << endl;
+            cin >> prezzo;
+            InsTesta(codice_fiscale, durata, tipo, prezzo);
+            break;
+
+        case 3:
+            cout << "Inserire l'abbonamento da inserire in fondo alla lista" << endl;
+            cout << "\nInserire il proprio codice fiscale:" << endl;
+            cin >> codice_fiscale;
+            cout << "\nInserire la durata dell'abbonamento:" << endl;
+            cin >> durata;
+            cout << "\nInserire il tipo di abbonamento:" << endl;
+            cin >> tipo;
+            cout << "\nInserire il prezzo dell'abbonamento:" << endl;
+            cin >> prezzo;
+            InsCoda(codice_fiscale, durata, tipo, prezzo);
+            break;
+
+        case 4:
+            CalcolaImportiTotali();
+            break;
+
+        default:
+            scelta = 0;
+            break;
+        }
+    } while (scelta != 0);
+
+    return 0;
+}
