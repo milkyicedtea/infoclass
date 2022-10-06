33f396c0f5e497431a11f8c63835779d2b691d84
Ignorance <erenblaze32@gmail.com>
2022-10-06 22:22:53 +0200

    Update Tests.cpp

--- Secondo anno/Tests/Tests.cpp
+++ Secondo anno/Tests/Tests.cpp
@@ -2,27 +2,11 @@
 
 using namespace std;
 
-struct Vini
-{
-    string denominazione;
-    string colore;
-    string origine;
-    int anno_imbottigliamento = 0;
-    float gradazione = 0.0;
-    float prezzo = 0.0;
-};
-
-Vini* pointer1 = NULL;
-
-Vini vino1;
-
-//Vini* arr[1]{pointer1};
+int x = 0;
 
 int main()
 {
-    vino1.prezzo = 10.0;
-    pointer1 = &vino1;
-    Vini* arr[1] {pointer1};
-    float max = arr[0]->prezzo;
-    cout << max;
+	cout << "x = " << x << ", " << "&x = " << &x << endl;
+	x++;
+	cout << "x = " << x << ", " << "&x = " << &x << endl;
 }
