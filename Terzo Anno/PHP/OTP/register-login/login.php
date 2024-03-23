6e51dec4866b3348bfe2ae4289bc2ee38e8e3270
Ignorance <81954613+milkyicedtea@users.noreply.github.com>
2024-03-23 12:48:50 +0100

    finished and working (styling?)

--- Terzo Anno/PHP/OTP/register-login/login.php
+++ Terzo Anno/PHP/OTP/register-login/login.php
@@ -1,68 +1,65 @@
 <?php
 	session_start();
-    require_once '../config.php';
-    require 'send_otp.php';
+	require_once '../config.php';
+	require 'send_otp.php';
 
 
+	function get_user_and_email ($userid)
+	{
+		$conn = getConn();
+		if ($stmt = $conn->prepare('select email from utenti where id_utente = ?')) {
+			$stmt->bind_param('s', $userid);
+			$stmt->execute();
+			$stmt->store_result();
 
-function get_user_and_email($userid) {
-    $conn = getConn();
-    if ($stmt = $conn->prepare('select username, email from utenti where id_utente = ?')) {
-        $stmt->bind_param('s', $userid);
-        $stmt->execute();
-        $stmt->store_result();
-
-        if ($stmt->num_rows > 0) {
-            $stmt->bind_result($username, $useremail);
-            $stmt->fetch();
-
-            return [$username, $useremail];
-        }
-    }
-}
+			if ($stmt->num_rows > 0) {
+				$stmt->bind_result($useremail);
+				$stmt->fetch();
+				echo '<script>console.log("' . $useremail . '")</script>';
+				return $useremail;
+			}
+		}
+	}
 
 // Check params presence
-if (!isset($_POST['username'], $_POST['password'])){
-    exit('Riempire tutti i campi.');
-}
+	if (!isset($_POST['username'], $_POST['password'])) {
+		exit('Riempire tutti i campi.');
+	}
 
-$conn = getConn();
+	$conn = getConn();
 
-if ($stmt = $conn->prepare('select id_utente, `password` from utenti where username = ?')){
-    // Bind parameters (s = string, i = int, etc)
-    $stmt->bind_param('s', $_POST['username']);
-    $stmt->execute();
-    $stmt->store_result();
+	if ($stmt = $conn->prepare('select id_utente, `password` from utenti where username = ?')) {
+		// Bind parameters (s = string, i = int, etc)
+		$stmt->bind_param('s', $_POST['username']);
+		$stmt->execute();
+		$stmt->store_result();
 
-    if ($stmt->num_rows > 0){
-        $stmt->bind_result($id_db, $password_db);
-        $stmt->fetch();
+		if ($stmt->num_rows > 0) {
+			$stmt->bind_result($id_db, $password_db);
+			$stmt->fetch();
 
-        // Account esiste, verifica password
-        if (password_verify($_POST['password'], $password_db)){
-            // Verifica completata!
-            // Crea sessione
-            session_regenerate_id();
-            $_SESSION['logged-in'] = TRUE;
-            $_SESSION['name'] = $_POST['username'];
-            $_SESSION['id'] = $id_db;
+			// Account esiste, verifica password
+			if (password_verify($_POST['password'], $password_db)) {
+				// Verifica completata!
+				// Crea sessione
+				session_regenerate_id();
+				$_SESSION['logged-in'] = TRUE;
+				$_SESSION['name'] = $_POST['username'];
+				$_SESSION['id'] = $id_db;
 
-            $userinfo = get_user_and_email($id_db); # userinfo[0] is the id, userinfo[1] is the email
+				$user_email = get_user_and_email($id_db); # userinfo[0] is the id, userinfo[1] is the email
 
-            send_otp($userinfo[0], $userinfo[1]);
-            header('location: otp.html');
-        }
-        else
-        {
-            // Password erraata
-            echo "<script>alert('Incorrect password!');location.href = 'login.html';}</script>";
-        }
-    }
-    else {
-        echo "<script>alert('Incorrect username or the account does not exist');location.href = 'login.html'</script>";
-    }
+				send_otp($id_db, $user_email);
+				header('location: otp.html');
+			}else{
+				// Password erraata
+				echo "<script>alert('Incorrect password!');location.href = 'login.html';}</script>";
+			}
+		}else{
+			echo "<script>alert('Incorrect username or the account does not exist');location.href = 'login.html'</script>";
+		}
 
-    $stmt->close();
-}
+		$stmt->close();
+	}
 
-$conn->close();
+	$conn->close();
